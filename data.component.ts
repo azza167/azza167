ec820cf537dca9b8da45b9c25c8ec62ebebe67d6
azza167 <113239568+azza167@users.noreply.github.com>
2022-09-26 12:01:39 +0200

    add dataComonent

---
+++ data.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.scss']
+})
+export class DataComponent implements OnInit {
+
+  inputValue: string = "";
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
